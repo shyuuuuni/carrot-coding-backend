85b00ecefef8bdee3070c59afd7bfe66b65cace0
Seung-hyun Kim <seunghyun4525@gmail.com>
2023-04-07 12:51:08 +0900

    feat: 자료구조 DB스키마를 DataStructureDetail로 통합

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,18 +1,6 @@
 // chat gpt api
 export type ChatGptRequestType = 'data-structure' | 'algorithm';
 
-// data-structure
-export type DataStructureName = string;
-export type ProgrammingLanguage = string;
-export type DataStructureDetails = {
-  Code: string;
-  Complexity: {
-    [key: string]: string;
-  };
-  Description: string;
-};
-export type DataStructureDetailsState = 'empty' | 'reported' | 'ok';
-
 // algorithm
 export type AlgorithmDetails = {
   Code: string;
