f4a0be09d15354605b6a65db19cfb33f7827868d
Seung-hyun Kim <seunghyun4525@gmail.com>
2023-05-03 12:33:11 +0900

    feat: ChatGPT 모듈을 서비스 레이어로 사용할 수 있도록 구조 변경

--- src/chat-gpt/chatGpt.repository.ts
+++ src/chat-gpt/chatGpt.repository.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { ChatGpt } from 'src/chat-gpt/chatGpt.schema';
-import { ChatGptRequestType } from 'src/types/types';
+import { ChatGpt, ChatGptRequest } from 'src/chat-gpt/chatGpt.schema';
+import {
+  ChatGptDomain,
+  ChatGptRequestType,
+  ChatGptType,
+} from 'src/types/types';
 
 @Injectable()
 export class ChatGptRepository {
   constructor(
     @InjectModel(ChatGpt.name)
     private chatGptModel: Model<ChatGpt>,
+    @InjectModel(ChatGptRequest.name)
+    private chatGptRequestModel: Model<ChatGptRequest>,
   ) {}
+
+  async findRequestOne(domain: ChatGptDomain, type: ChatGptType) {
+    const document = await this.chatGptRequestModel
+      .findOne({ domain, type })
+      .exec();
+
+    return document;
+  }
 
   async getGenerationQuestion(requestType: ChatGptRequestType) {
     const generationQuestion = await this.chatGptModel
